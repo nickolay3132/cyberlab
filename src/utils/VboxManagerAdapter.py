352acf58d08109c898fd833123d7a49af1518649
nickolay3132 <nickolay3132@gmail.com>
2025-06-27 11:50:36 +0300

    added methods to the VBoxManageAdapter class for future startup and shutdown commands

--- src/utils/VboxManagerAdapter.py
+++ src/utils/VboxManagerAdapter.py
@@ -1,4 +1,5 @@
 import subprocess
+from typing import Literal
 
 
 class VboxManagerAdapter:
@@ -6,7 +7,7 @@ class VboxManagerAdapter:
     def import_vm(ova_path: str, vm_name: str, vms_dir: str) -> bool:
         print(f"\nImporting {vm_name}...")
         cmd = [
-            "vboxmanage", "import", ova_path,
+            "VBoxManage", "import", ova_path,
             "--vsys", "0",
             "--vmname", vm_name,
             "--group", "/cyberlab",
@@ -18,3 +19,26 @@ class VboxManagerAdapter:
         process.wait()
 
         return process.returncode == 0
+
+    @staticmethod
+    def start_vm(vm_name: str, launch_type: Literal["gui", "headless", "separate"] = "gui") -> bool:
+        cmd = [
+            "VBoxManage", "startvm", vm_name, "--type", launch_type,
+        ]
+
+        process = subprocess.Popen(cmd)
+        process.wait()
+        return process.returncode == 0
+
+    @staticmethod
+    def stop_vm(vm_name: str, shutdown_type: Literal["acpipowerbutton", "poweroff", "savestate"]) -> bool:
+        print(f"\nStopping {vm_name}...")
+        cmd = [
+            "VBoxManage", "controlvm", vm_name, shutdown_type
+        ]
+
+        process = subprocess.Popen(cmd)
+        process.wait()
+
+        return process.returncode == 0
+
