72a70709fb8ba2bb89028054b9a76b8f74ab36b4
DARKFU1 <d_a_r_k_f_u_l@mail.ru>
2025-07-02 11:04:52 +0300

    Cleanup

--- src/commands/install.py
+++ src/commands/install.py
@@ -4,7 +4,8 @@ from urllib.parse import urljoin
 
 from src import configuration
 from src.utils.Utils import Utils
-from src.utils.VboxManagerAdapter import VboxManagerAdapter
+# from src.utils.VboxManagerAdapter import VboxManagerAdapter
+from src.utils.VboxManagerAdapter import ConcurrentImporter
 
 import threading
 
@@ -32,34 +33,22 @@ def run(args):
 
     print("\n=== IMPORTING VMS ===")
 
-    tasks = []
     for vm_name, filepath in Utils.find_files(ova_dir,".ova"):
         try:
             if os.path.exists(os.path.join(vms_dir, vm_name)):
                 print(f"{vm_name} already imported! Skipping...")
 
-            tasks.append([vm_name, threading.Thread(
-                        target=VboxManagerAdapter.import_vm,
-                        args=(filepath, vm_name, vms_dir))])
+            ConcurrentImporter.import_vm(filepath, vm_name, vms_dir)
 
         except Exception as e:
             print(f"\nError importing {vm_name}: {str(e)}")
 
-    for [name, thread] in tasks:
-        thread.start()
+    ConcurrentImporter.wait()
 
-    is_all_completed = False
-    while not is_all_completed:
-        for index, [name, thread] in enumerate(tasks):
-            if not thread.is_alive():
-                print("*** {:^60} ***".format(f"{name} finished importing!"))
-                del tasks[index]
-                break
-        if len(tasks) == 0:
-            is_all_completed = True
-            break
-
-        time.sleep(0.25)
-
+    for index, result in enumerate(ConcurrentImporter.results):
+        if result == False:
+            print("\nError importing {name}: {err}"
+                  .format(ConcurrentImporter.names[index],
+                          ConcurrentImporter.errors[index]))
 
     print("\nAll operations completed!")
